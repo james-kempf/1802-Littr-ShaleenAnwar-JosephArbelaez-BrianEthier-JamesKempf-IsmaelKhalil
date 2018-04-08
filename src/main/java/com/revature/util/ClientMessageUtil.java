2f952aa06547471c230cba316d47e3c25f38aa9c
Joseph <30915062+Xeroph@users.noreply.github.com>
2018-04-08 17:25:10 -0400

    Fixed Errors

--- src/main/java/com/revature/util/ClientMessageUtil.java
+++ src/main/java/com/revature/util/ClientMessageUtil.java
@@ -7,7 +7,7 @@ public class ClientMessageUtil {
 	public static final ClientMessage SOMETHING_WRONG = new ClientMessage("SOMETHING WENT WRONG", false);
 	public static final ClientMessage INSERT_SUCCESSFUL = new ClientMessage("INSERT SUCCESSFUL", true);
 	public static final ClientMessage DELETE_SUCCESSFUL = new ClientMessage("DELETE SUCCESSFUL", true); 
-	static final ClientMessage FRIENDSHIP_APPROVED = new ClientMessage("FRIENDSHIP APPROVED", true);
+	public static final ClientMessage FRIENDSHIP_APPROVED = new ClientMessage("FRIENDSHIP APPROVED", true);
 	public static final ClientMessage FRIENDSHIP_DELETED = new ClientMessage("FRIENDSHIP DELETED", true);
 	public static final ClientMessage LOGIN_SUCCESSFUL = new ClientMessage("LOGIN SUCCESSFUL", true);
 }
