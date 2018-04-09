ea4464ae9713a3470d962394e06e4ac8b7b6d75c
shaleen <shaleen.anwar@gmail.com>
2018-04-09 11:08:26 -0400

    Logic added to CatInfoController and new Client Message added

--- src/main/java/com/revature/util/ClientMessageUtil.java
+++ src/main/java/com/revature/util/ClientMessageUtil.java
@@ -4,6 +4,7 @@ import com.revature.ajax.ClientMessage;
 
 public class ClientMessageUtil {
 	public static final ClientMessage REGISTRATION_SUCCESSFUL = new ClientMessage("REGISTRATION SUCCESSFUL", true);
+	public static final ClientMessage UPDATE_SUCCESSFUL = new ClientMessage("UPDATE SUCCESSFUL", true);
 	public static final ClientMessage SOMETHING_WRONG = new ClientMessage("SOMETHING WENT WRONG", false);
 	public static final ClientMessage INSERT_SUCCESSFUL = new ClientMessage("INSERT SUCCESSFUL", true);
 	public static final ClientMessage DELETE_SUCCESSFUL = new ClientMessage("DELETE SUCCESSFUL", true); 
