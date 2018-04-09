8a14108547a9f2e31cd0c73e6db7c1ae3d607175
James Kempf <jamesk4321@gmail.com>
2018-04-09 19:45:26 -0400

    switch

---
+++ littr-client/src/app/components/view-friends/view-friends.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-friends',
+  templateUrl: './view-friends.component.html',
+  styleUrls: ['./view-friends.component.css']
+})
+export class ViewFriendsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
