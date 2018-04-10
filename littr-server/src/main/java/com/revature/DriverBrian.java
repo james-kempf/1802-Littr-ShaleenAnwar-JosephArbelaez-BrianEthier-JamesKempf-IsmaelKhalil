1118ab97f52d603a67277d12849ae3ad4e773549
Brian Ethier <brianethier111@gmail.com>
2018-04-10 13:35:48 -0400

    code cleanup

--- littr-server/src/main/java/com/revature/DriverBrian.java
+++ littr-server/src/main/java/com/revature/DriverBrian.java
@@ -33,19 +33,6 @@ public class DriverBrian {
 	logger.info("atlas: " + atlas);
 	Cat koushka = catService.findCatByName("koushka");
 	logger.info("koushka: " + koushka);
-	
-	/*
-	Friendship emackAtlas = new Friendship(
-			emack, atlas,
-			new FriendStatus(2,"APPROVED"),
-			Timestamp.valueOf(LocalDateTime.now()));
-	logger.info("emackAtlas: " + emackAtlas);
-	friendshipService.insertFriendship(emackAtlas);
-	 
-	logger.info("emackAtlas: " + emackAtlas);
-	*/
-	//logger.info("friends?" + friendshipService.findFriendshipByCat(emack, atlas));
-	//logger.info("friends?" + friendshipService.findFriendshipByCat(atlas, emack));
 	logger.info("friends?" + friendshipService.findFriendshipByCat(atlas, koushka));
 	
 	Post koushPost = new Post(1, koushka, 
@@ -55,8 +42,6 @@ public class DriverBrian {
 	Post koushPost2 = new Post(1, koushka, 
 			Timestamp.valueOf(LocalDateTime.now()),
 			null, "A random Koushka Appears");
-	//logger.info("insert koush post 1: " + postService.insertPost(koushPost));
-	//logger.info("insert koush post 2: " + postService.insertPost(koushPost2));
 	
 	logger.info("koushka looking at koushka post: " + postService.findPostsByCat(koushka, koushka));
 	logger.info("atlas looking at koushka post: " + postService.findPostsByCat(atlas, koushka));
