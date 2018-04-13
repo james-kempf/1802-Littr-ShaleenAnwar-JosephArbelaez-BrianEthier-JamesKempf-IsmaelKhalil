7f600cc1b9b0bc4e89bdf1e44feb52a1fe857567
shaleen <shaleen.anwar@gmail.com>
2018-04-13 10:37:04 -0400

    Fixing errors in CatInformationController

--- littr-server/src/main/java/com/revature/controller/CatInformationController.java
+++ littr-server/src/main/java/com/revature/controller/CatInformationController.java
@@ -7,6 +7,6 @@ import com.revature.model.Cat;
 
 public interface CatInformationController {
 	ClientMessage registerCat(Cat cat);
-	Cat getCat(String catName);
+	Cat getCat(Cat cat);
 	ClientMessage updateCat(Cat cat);
 }
